4a0c5978fa7e8392291b5ea5d386c22f6f84fc77
gypsy0ne <zoomik100@gmail.com>
2021-06-06 19:38:35 +0200

    unit tests

--- AbstractFactory/src/test/kotlin/RegularMazeFactoryTest.kt
+++ AbstractFactory/src/test/kotlin/RegularMazeFactoryTest.kt
@@ -3,26 +3,27 @@ import model.Room
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
-class PuzzledMazeFactoryTest {
+class RegularMazeFactoryTest {
 
-    private val factory = PuzzledMazeFactory()
+    private val factory = RegularMazeFactory()
 
     @Test
-    fun `should factory create locked door`() {
+    fun `should factory create regular door`() {
         // given
         val roomFrom = factory.makeRoom(2)
         val roomTo = factory.makeRoom(1)
         val door = factory.makeDoor(roomFrom = roomFrom, roomTo = roomTo)
+
         // then
-        assertThat(door).isInstanceOf(Door.LockedDoor::class.java)
+        assertThat(door).isInstanceOf(Door.RegularDoor::class.java)
     }
 
     @Test
-    fun `should factory create room with traps`() {
+    fun `should factory create regular room`() {
         // given
         val room = factory.makeRoom(2)
 
         // then
-        assertThat(room).isInstanceOf(Room.RoomWithTrap::class.java)
+        assertThat(room).isInstanceOf(Room.RegularRoom::class.java)
     }
 }
