008b5b5aa738590015f1fccacfb2e03a874b6e6f
Rafal Kapela <rafal.kapela@allegro.pl>
2021-08-22 20:23:14 +0200

    theory

--- Singleton/src/main/kotlin/RegularMazeFactory.kt
+++ Singleton/src/main/kotlin/RegularMazeFactory.kt
@@ -1,8 +1,4 @@
-import model.Door
-import model.Maze
-import model.Room
-
 internal sealed class MazeFactory {
-    object BombedMazeFactory : MazeFactory {}
-    object PuzzledMazeFactory : MazeFactory {}
+    object BombedMazeFactory : MazeFactory() {}
+    object PuzzledMazeFactory : MazeFactory() {}
 }
