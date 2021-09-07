6ff6b4c47b4b94ee0be61aff474231633c54c9a3
Rafal Kapela <rafal.kapela@allegro.pl>
2021-09-07 09:19:31 +0200

    code simplification

--- Bridge/src/main/kotlin/Window.kt
+++ Bridge/src/main/kotlin/Window.kt
@@ -5,20 +5,7 @@ import model.View
 
 internal abstract class Window {
     open fun drawContents() {}
-    open fun open() {}
-    open fun close() {}
-    open fun iconify() {}
-    open fun deiconify() {}
-
-    open fun setOrigin(at: Point) {}
-    open fun setExtent(extent: Point) {}
-    open fun raise() {}
-    open fun lower() {}
-
-    open fun drawLine(from: Point, to: Point) {}
     open fun drawRect(upperLeft: Point, bottomRight: Point) = windowImp.deviceRect(upperLeft, bottomRight)
-    open fun drawPolygon(vertexes: List<Point>) {}
-    open fun drawText(at: Point, text: String) {}
 
     protected val windowImp: WindowImp = WindowSystemFactory.makeWindowImpl()
     protected abstract val contents: View
